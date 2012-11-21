2c5bf0a79a39b29362b7c705aa10d8325783cabc
Matthew Courtney <macourtney@gmail.com>
2012-11-21 13:27:30 -0500

    Updated clj-crypto to work on my linux install.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
   :dependencies [[bouncycastle/bcprov-jdk16 "140"]
                  [commons-codec/commons-codec "1.5"]
-                 [org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.2.0"]])
