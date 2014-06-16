78eb9c3160a2c1896c193a112354628b62847774
Matt Courtney <macourtney@gmail.com>
2014-06-15 19:24:55 -0700

    Updated to the latest version of bouncy castle.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-crypto "1.0.1-SNAPSHOT"
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
-  :dependencies [[bouncycastle/bcprov-jdk16 "140"]
+  :dependencies [[org.bouncycastle/bcprov-jdk16 "1.46"]
                  [commons-codec/commons-codec "1.5"]
                  [org.clojure/tools.logging "0.2.0"]])
