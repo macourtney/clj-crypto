881a0fe1ff52908d6b73f0462866dd8afa414b40
Alexis <arodrig@fing.edu.uy>
2014-08-06 11:41:01 -0300

    extract a KeyPair from keystore
    Added get-key-pair-pkcs12 function to core.clj. This function loads the keypair
    from certain entry alias in a pkcs12 keystore.
    
    This commit also adds org.clojure/tools.logging dependency to the project.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
   :dependencies [[org.bouncycastle/bcprov-jdk15on "1.50"]
                  [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [commons-codec/commons-codec "1.5"]])
