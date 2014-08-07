542cc886e92bf9daf640d7dbb7e8e478aee86d13
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-08-07 10:49:19 -0300

    Bumped up version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.guilespi/clj-crypto "1.0.1"
+(defproject org.clojars.guilespi/clj-crypto "1.0.2"
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
   :dependencies [[org.bouncycastle/bcprov-jdk15on "1.50"]
                  [org.clojure/clojure "1.6.0"]
