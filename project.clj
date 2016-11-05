b230e5359bb902ec396fefb762b1c20041e1a543
J. Pablo Fernández <pupeno@pupeno.com>
2016-11-05 01:14:38 +0000

    Upgrade to Bouncy Castle 1.55. All tests seem to pass.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-crypto "1.0.3-SNAPSHOT"
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
-  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.50"]
+  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.55"]
                  [commons-codec/commons-codec "1.5"]
                  [org.clojure/tools.logging "0.3.0"]])
